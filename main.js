9a0b0280db3def402cedec85e913db0127244b28
Anna Śmierzchała <smierzchala.anna@gmail.com>
2020-05-09 18:06:37 +0200

    add new articles and new styles

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ console.log(`Hej, nazywam się ${firstName} i mam ${age} lat`);
 
 
 
+const heading = document.querySelector('.heading__paragraph--js');
+
+heading.innerHTML = 'Tu miał być tekst moje notatki'
+
+console.log(heading);
+console.log(heading.innerHTML);
