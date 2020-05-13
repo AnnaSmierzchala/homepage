4dddf79bc4e632c4c5dcfb0cda25240a9e4e6353
Anna Śmierzchała <smierzchala.anna@gmail.com>
2020-05-13 19:10:42 +0200

    background and new styles added

--- main.js
+++ main.js
@@ -7,9 +7,41 @@ console.log(`Hej, nazywam się ${firstName} i mam ${age} lat`);
 
 
 
-const heading = document.querySelector('.heading__paragraph--js');
 
-heading.innerHTML = 'Tu miał być tekst moje notatki'
 
-console.log(heading);
-console.log(heading.innerHTML);
+
+
+function greet(age, firstName) {
+    console.log(`Hej, nazywam się ${firstName} i mam ${age} lat`);
+    
+}
+
+function createContent(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent);
+
+    element.innerHTML = content;
+} 
+
+createContent(`.heading__paragraph--js`, 'Witaj świecie');
+
+const deathStar = {
+    diameter: 120000,
+    fire: (target) => {
+        console.log(`${target} destroyed`)
+    },
+    isOperating: true,
+    levels: 357,
+    name: 'Death Star',
+    population: 10000,
+    isLightOn: true,
+    comander: {
+        name: 'Darth Vader',
+        age: 44,
+    }
+}
+const myProperty = 'name';
+const showMeProperty = (myProperty) => {
+    console.log(`Twoja własność ${myProperty} to: ${deathStar[myProperty]}`);
+}
+
+showMeProperty('levels')
